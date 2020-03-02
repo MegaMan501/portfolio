c98adb29a5b4dde842a8b9630149dd6e07f0797b
MegaMan501 <mohamed.rahaman501@gmail.com>
2020-03-02 03:07:41 -0600

    change to css grid for layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+import { MastheadComponent } from './masthead/masthead.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { BioComponent } from './bio/bio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MastheadComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +40,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
